02b1dce52bdc3ec1ace4413feb3fdd2b90377cdb
Devon <devon.morancie@gmail.com>
2021-12-09 10:16:09 +0200

    Rename references...debugging

--- 1-setup.sh
+++ 1-setup.sh
@@ -244,14 +244,14 @@ fi
 echo -e "\nDone!\n"
 if ! source install.conf; then
 	read -p "Please enter username:" username
-echo "username=$username" >> ${HOME}/ArchForce/install.conf
+echo "username=$username" >> ${HOME}/ArchTitus/install.conf
 fi
 if [ $(whoami) = "root"  ];
 then
     useradd -m -G wheel,libvirt -s /bin/bash $username 
 	passwd $username
-	cp -R /root/ArchForce /home/$username/
-    chown -R $username: /home/$username/ArchForce
+	cp -R /root/ArchTitus /home/$username/
+    chown -R $username: /home/$username/ArchTitus
 	read -p "Please name your machine:" nameofmachine
 	echo $nameofmachine > /etc/hostname
 else
