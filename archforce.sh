61c0de03624ed1d5d5ba7175af3c77556626ad34
Devon <devon.morancie@gmail.com>
2021-12-09 14:25:36 +0200

    Rename directories and stuff

--- archforce.sh
+++ archforce.sh
@@ -8,19 +8,19 @@
     echo "Setup in 3 Seconds ..." && sleep 1
     echo "Setup in 2 Seconds ..." && sleep 1
     echo "Setup in 1 Second ..." && sleep 1    
-    arch-chroot /mnt /root/ArchTitus/1-setup.sh
+    arch-chroot /mnt /root/ArchForce/1-setup.sh
 
     echo "Install conf in 3 Seconds ..." && sleep 1
     echo "Install conf in 2 Seconds ..." && sleep 1
     echo "Install conf in 1 Second ..." && sleep 1    
-    source /mnt/root/ArchTitus/install.conf
+    source /mnt/root/ArchForce/install.conf
 
     echo "Mount in 3 Seconds ..." && sleep 1
     echo "Mount in 2 Seconds ..." && sleep 1
     echo "Mount in 1 Second ..." && sleep 1    
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchForce/2-user.sh
 
     echo "Post setup in 3 Seconds ..." && sleep 1
     echo "Post setup in 2 Seconds ..." && sleep 1
     echo "Post setup in 1 Second ..." && sleep 1    
-    arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
+    arch-chroot /mnt /root/ArchForce/3-post-setup.sh
