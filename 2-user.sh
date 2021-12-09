30476ebefed0431034434a102a7af58a56197ff2
Devon <devon.morancie@gmail.com>
2021-12-09 17:04:33 +0200

    Add root to pacman for java download

--- 2-user.sh
+++ 2-user.sh
@@ -68,7 +68,7 @@ for PKG in "${PKGS[@]}"; do
 done
 
 # Download JDK 11
- pacman  -Suy jre11-openjdk
+sudo pacman -Suy jre11-openjdk
 # Set default java version to Java 11
 archlinux-java set java-11-openjdk
 
