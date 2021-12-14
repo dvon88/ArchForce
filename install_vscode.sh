72b829a7466ad420620e358218dc4fb6c6059a46
Devon <devon.morancie@gmail.com>
2021-12-14 10:02:03 +0200

    Update Salesforce extension pack installation

--- install_vscode.sh
+++ install_vscode.sh
@@ -4,12 +4,6 @@
 yay -S --noconfirm --needed visual-studio-code-bin
 # Install extensions for VSCode
 code --isntall-extension dbaeumer.vscode-eslint
-code --install-extension salesforce.salesforce-vscode-slds
-code --install-extension salesforce.salesforcedx-vscode-apex
-code --install-extension salesforce.salesforcedx-vscode-apex-replay-debugger
-code --install-extension salesforce.salesforcedx-vscode-core
-code --install-extension salesforce.salesforcedx-vscode-lightning
-code --install-extension salesforce.salesforcedx-vscode-soql
-code --install-extension salesforce.salesforcedx-vscode-visualforce
+code --install-extension salesforce.salesforcedx-vscode
 code --install-extension eamodio.gitlens
 code --install-extension mhutchie.git-graph
