689a54c222f4684ce85542006fed1a4657354b64
Devon <devon.morancie@gmail.com>
2021-12-14 10:54:13 +0200

    Fix spelling error

--- install_vscode.sh
+++ install_vscode.sh
@@ -3,7 +3,7 @@
 # Install VSCode
 yay -S --noconfirm --needed visual-studio-code-bin
 # Install extensions for VSCode
-code --isntall-extension dbaeumer.vscode-eslint
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension salesforce.salesforcedx-vscode
 code --install-extension eamodio.gitlens
 code --install-extension mhutchie.git-graph
